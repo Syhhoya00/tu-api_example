90d1a9d6a6cbdf90505e5af857a034349239152e
massivefox <rlwkdwhddls@naver.com>
2023-04-18 14:07:32 +0900

    backTest

--- index.js
+++ index.js
@@ -1,75 +1,109 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const url = process.env.APIURL;
+const axios = require('axios').default;
 const port = process.env.PORT;
 const Caver = require("caver-js");
 const caver = new Caver(`https://api.baobab.klaytn.net:8651`);
 
-console.log('changeTest')
+
 app.use(express.json());
 
 app.listen(port, () => {
   console.log("listening...", port);
 });
-console.log(caver)
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
-// 계정 잔고 확인
-app.get("/getbalance", async (req, res) => {
+app.post("/metadata", async (req, res) => {
   try{
-  console.log(req.query.address)
-  const balance = await caver.klay.getBalance(req.query.address);
-  console.log(balance)
-  const convert = caver.utils.convertFromPeb(balance, "KLAY");
-  res.json(`잔고 : ${convert}`);
+ 
+  const balance = await testCode(req.body.metadata)
+  res.json(balance);
 }catch(e){
   console.log(e)
 }
 });
+async function testCode(metadata) {
 
-// 가장 최근에 생성된 블록 번호 확인
-app.get("/getblock", async (req, res) => {
-  const block = await caver.klay.getBlockNumber();
-  res.json(block);
-console.log(res)
-});
+  const res = await axios.post(
+    `${url}/v1/metadata`,
+    {
+      metadata,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Basic S0FTSzNWSzI5WEFFVkdKWEZFUkpSOFlROmRJVlFwZG9yV1BFakc5MzIwUVVtb1BiclRhZnRNQ0RZTFE5VDBQeE8=",
+        "x-chain-id": "1001",
+      },
+    }
+  );
+  return res.data;
+}
 
-// EOA로 Klaytn 계정 정보 확인
-app.get("/getAccount", async (req, res) => {
-  const account = await caver.klay.getAccount(req.query.address);
-  res.json(account);
-});
 
-// 클레이튼을 다른 주소로 전송
-app.get("/transfer", async (req, res) => {
-  //   const sender = req.query.sender;
-  const receiver = req.query.receiver;
-  const amount = req.query.amount;
 
-  let balance = await caver.klay.getBalance(process.env.ADDRESS);
-  balance = caver.utils.fromPeb(balance, "KLAY");
-  console.log(balance);
 
-  if (balance > amount) {
-    const keyring = caver.wallet.keyring.createFromPrivateKey(
-      process.env.PRIVATE_KEY
-    );
-    const valueTransfer = caver.transaction.valueTransfer.create({
-      from: keyring.address,
-      to: receiver,
-      value: caver.utils.toPeb(`${amount}`, "KLAY"),
-      gas: 30000,
-    });
-    // console.log(valueTransfer);
 
-    const signed = await valueTransfer.sign(keyring);
-    console.log(`signed: ${signed}`);
-    const receipt = await caver.rpc.klay.sendRawTransaction(signed);
 
-    res.send(receipt);
-  } else {
-    res.status(400).send("transfer fail");
-  }
-});
+
+
+
+// // 계정 잔고 확인
+// app.get("/getbalance", async (req, res) => {
+//   try{
+//   console.log(req.query.address)
+//   const balance = await caver.klay.getBalance(req.query.address);
+//   console.log(balance)
+//   const convert = caver.utils.convertFromPeb(balance, "KLAY");
+//   res.json(`잔고 : ${convert}`);
+// }catch(e){
+//   console.log(e)
+// }
+// });
+
+// // 가장 최근에 생성된 블록 번호 확인
+// app.get("/getblock", async (req, res) => {
+//   const block = await caver.klay.getBlockNumber();
+//   res.json(block);
+// console.log(res)
+// });
+
+// // EOA로 Klaytn 계정 정보 확인
+// app.get("/getAccount", async (req, res) => {
+//   const account = await caver.klay.getAccount(req.query.address);
+//   res.json(account);
+// });
+
+// // 클레이튼을 다른 주소로 전송
+// app.get("/transfer", async (req, res) => {
+//   //   const sender = req.query.sender;
+//   const receiver = req.query.receiver;
+//   const amount = req.query.amount;
+
+//   let balance = await caver.klay.getBalance(process.env.ADDRESS);
+//   balance = caver.utils.fromPeb(balance, "KLAY");
+//   console.log(balance);
+
+//   if (balance > amount) {
+//     const keyring = caver.wallet.keyring.createFromPrivateKey(
+//       process.env.PRIVATE_KEY
+//     );
+//     const valueTransfer = caver.transaction.valueTransfer.create({
+//       from: keyring.address,
+//       to: receiver,
+//       value: caver.utils.toPeb(`${amount}`, "KLAY"),
+//       gas: 30000,
+//     });
+//     // console.log(valueTransfer);
+
+//     const signed = await valueTransfer.sign(keyring);
+//     console.log(`signed: ${signed}`);
+//     const receipt = await caver.rpc.klay.sendRawTransaction(signed);
+
+//     res.send(receipt);
+//   } else {
+//     res.status(400).send("transfer fail");
+//   }
+// });
